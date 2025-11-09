ad6196bbf22fb2081643351f31e6c099bc3b057f
NicoG2023 <nguevarah@udistrital.edu.co>
2025-11-08 19:10:57 -0500

    Added Unit Testing for Java backend

--- Backend/Java/authentication/src/main/java/org/auth/resources/AuthAdminResource.java
+++ Backend/Java/authentication/src/main/java/org/auth/resources/AuthAdminResource.java
@@ -16,9 +16,7 @@ import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.media.ExampleObject;
+import org.eclipse.microprofile.openapi.annotations.media.*;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
@@ -38,8 +36,14 @@ import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 @SecurityRequirement(name = "bearerAuth")
 public class AuthAdminResource {
 
-    @Inject KcAdminService kc;
-    @Inject SecurityIdentity identity;
+    private final KcAdminService kc;
+    private final SecurityIdentity identity;
+
+    @Inject
+    public AuthAdminResource(KcAdminService kc, SecurityIdentity identity) {
+        this.kc = kc;
+        this.identity = identity;
+    }
 
     /* SCHEMAS */
 
